b05cd85c89afb68477d20dcd6a0bf30640d145dc
0oSaraho0 <0osarahtoddo0@gmail.com>
2022-08-14 18:22:29 +0000

    add style to to bag

--- bag/contexts.py
+++ bag/contexts.py
@@ -3,13 +3,12 @@ from django.conf import settings
 from django.shortcuts import get_object_or_404
 from products.models import Product
 
-
 def bag_contents(request):
 
     bag_items = []
     total = 0
     product_count = 0
-    bag = request.session.get('bag',{})
+    bag = request.session.get('bag', {})
 
     for item_id, quantity in bag.items():
         product = get_object_or_404(Product, pk=item_id)
@@ -18,18 +17,18 @@ def bag_contents(request):
         bag_items.append({
             'item_id': item_id,
             'quantity': quantity,
-            'products': product,
+            'product': product,
         })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
-        delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE)
+        delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
         free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
     else:
         delivery = 0
         free_delivery_delta = 0
-
+    
     grand_total = delivery + total
-
+    
     context = {
         'bag_items': bag_items,
         'total': total,
