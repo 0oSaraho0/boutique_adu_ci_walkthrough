b5a12d14ae3b89992dee46af9e71aaf7819bf67a
0oSaraho0 <0osarahtoddo0@gmail.com>
2022-09-01 14:27:17 +0000

    product admin - product form

--- profiles/views.py
+++ profiles/views.py
@@ -7,13 +7,11 @@ from .forms import UserProfileForm
 from checkout.models import Order
 
 def profile(request):
-    """ Display the user's profile"""
-
-    profile = get_object_or_404(UserProfile, user=request.user) 
-
+    """ Display the user's profile. """
+    profile = get_object_or_404(UserProfile, user=request.user)
 
     if request.method == 'POST':
-        form = UserProfileForm(requset.POST, instance=profile)
+        form = UserProfileForm(request.POST, instance=profile)
         if form.is_valid():
             form.save()
             messages.success(request, 'Profile updated successfully')
@@ -23,20 +21,19 @@ def profile(request):
 
     template = 'profiles/profile.html'
     context = {
-        
         'form': form,
         'orders': orders,
-        'on_profile_page': True 
+        'on_profile_page': True
     }
 
     return render(request, template, context)
-    
+
 
 def order_history(request, order_number):
     order = get_object_or_404(Order, order_number=order_number)
 
     messages.info(request, (
-        f'This is a past confirmation for order number {order_number}.'
+        f'This is a past confirmation for order number {order_number}. '
         'A confirmation email was sent on the order date.'
     ))
 
